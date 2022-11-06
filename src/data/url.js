2c7de096bb740b7f8c5309127829cd0860c6636e
Mohindran S.R <s.r.mohindran@gmail.com>
2022-11-06 19:30:27 +0530

    https

--- src/data/url.js
+++ src/data/url.js
@@ -2,13 +2,13 @@
 
 const env='prod'
 
-const condition=env=="local"?"http://localhost:5000/condition":"http://65.0.233.87:5000/condition"
-const getpatient=env=="local"?"http://localhost:5000/getpatient":"http://65.0.233.87:5000/getpatient"
-const getpatientbyid=env=="local"?"http://localhost:5000/getpatientbyid":"http://65.0.233.87:5000/getpatientbyid"
-const getpatientbyemail=env=="local"?"http://localhost:5000/getpatientbyemail":"http://65.0.233.87:5000/getpatientbyemail"
-const getconditionbydrug=env=="local"?"http://localhost:5000/drug":"http://65.0.233.87:5000/drug"
-const uploading=env=="local"?"http://localhost:5000/upload":"http://65.0.233.87:5000/upload"
-const sendemail=env=="local"?"http://localhost:5000/sendemail":"http://65.0.233.87:5000/sendemail"
+const condition=env=="local"?"http://localhost:5000/condition":"https://65.0.233.87:5000/condition"
+const getpatient=env=="local"?"http://localhost:5000/getpatient":"https://65.0.233.87:5000/getpatient"
+const getpatientbyid=env=="local"?"http://localhost:5000/getpatientbyid":"https://65.0.233.87:5000/getpatientbyid"
+const getpatientbyemail=env=="local"?"http://localhost:5000/getpatientbyemail":"https://65.0.233.87:5000/getpatientbyemail"
+const getconditionbydrug=env=="local"?"http://localhost:5000/drug":"https://65.0.233.87:5000/drug"
+const uploading=env=="local"?"http://localhost:5000/upload":"https://65.0.233.87:5000/upload"
+const sendemail=env=="local"?"http://localhost:5000/sendemail":"https://65.0.233.87:5000/sendemail"
 
 const urlMap={
 condition:condition,
